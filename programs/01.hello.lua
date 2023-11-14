1332f4fad622b007993073d16ab5d44b82dcdc16
arjunsaud <arundk055@gmail.com>
2023-11-14 23:30:37 +0545

    basic data

---
+++ programs/01.hello.lua
@@ -0,0 +1,11 @@
+-- this is comment
+--[[
+    this is multi line comment
+]]
+print("Hello")
+
+
+-- To concatinate the sting we use `..`
+print("Hello" .. " World")
+
+
---
+++ programs/02.Datatype.lua
@@ -0,0 +1,13 @@
+-- Datatypes
+--[[
+    nil  (this is empty value)
+    number (1,2,3,...0)
+    string ("hello")
+    boolean (true,false)
+    table
+]]
+
+local a=2
+print(a)
+print(a+5)
+print('My age is : ' .. a)
